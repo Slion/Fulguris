066a0787b7c9166c33ac76e854ab7689a84af4a7
Helium314 <43007630+Helium314@users.noreply.github.com>
2021-05-30 08:39:10 +0200

    remove domain-settings file if it's empty

--- app/src/main/java/acr/browser/lightning/database/DomainSettings.kt
+++ app/src/main/java/acr/browser/lightning/database/DomainSettings.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.net.Uri
 
-// domain settings backed by a shared preferences file
+// domain settings backed by one shared preferences file per domain
 //  use for persistent storage of sites for dark mode, blocking/allowing js or ads, maybe other things
 class DomainSettings(applicationContext: Context, url: String) {
     val prefs: SharedPreferences = applicationContext.getSharedPreferences(Uri.parse(url).host ?: "default", Context.MODE_PRIVATE)
@@ -19,6 +19,8 @@ class DomainSettings(applicationContext: Context, url: String) {
 
     fun remove(setting: String) {
         prefs.edit().remove(setting).apply()
+        if (prefs.all.isEmpty())
+            clear() // remove file if we removed the last preference
     }
 
     fun clear() = prefs.edit().clear().apply()
