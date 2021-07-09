5e9da242808031cf6fd4740e1acc0a7a80d6e3b8
Helium <helium314@disroot.org>
2021-07-09 22:56:52 +0200

    fix test with unMock, and extend tests

--- app/src/test/java/acr/browser/lightning/adblock/AbpBlockerTest.kt
+++ app/src/test/java/acr/browser/lightning/adblock/AbpBlockerTest.kt
@@ -1,22 +1,30 @@
 package acr.browser.lightning.adblock
 
+import android.net.Uri
+import android.webkit.WebResourceRequest
+import androidx.core.net.toUri
+import androidx.core.util.PatternsCompat
+import jp.hazuki.yuzubrowser.adblock.core.ContentRequest
+import jp.hazuki.yuzubrowser.adblock.core.FilterContainer
 import jp.hazuki.yuzubrowser.adblock.filter.abp.AbpFilterDecoder
 import jp.hazuki.yuzubrowser.adblock.filter.unified.UnifiedFilter
 import jp.hazuki.yuzubrowser.adblock.filter.unified.UnifiedFilterSet
 import jp.hazuki.yuzubrowser.adblock.filter.unified.io.FilterReader
 import jp.hazuki.yuzubrowser.adblock.filter.unified.io.FilterWriter
+import jp.hazuki.yuzubrowser.adblock.getContentType
+import okhttp3.internal.publicsuffix.PublicSuffix
 import org.junit.Test
 import java.io.ByteArrayOutputStream
 import java.io.File
 import org.junit.Assert.*
 
 class AbpBlockerTest {
+    var set: UnifiedFilterSet
+    val filters = mutableListOf<Pair<String, UnifiedFilter>>()
 
-    @Test
-    fun readWriteDecode() {
+    init {
         val ASSET_BASE_PATH = "../app/src/main/assets/"
         val easylist = File(ASSET_BASE_PATH + "easylist.txt")
-        lateinit var set: UnifiedFilterSet
         easylist.inputStream().bufferedReader().use {
             val decoder = AbpFilterDecoder()
             set = decoder.decode(it, null)
@@ -28,13 +36,60 @@ class AbpBlockerTest {
             writer.write(it, set.blackList)
         }
 
-        val filters = mutableListOf<UnifiedFilter>()
         filterStore.toByteArray().inputStream().use { stream ->
             val reader = FilterReader(stream)
             if (reader.checkHeader())
-                filters.addAll(reader.readAll().map {it.second})
+                filters.addAll(reader.readAll())
         }
-
-        assertEquals(set.blackList, filters)
     }
+
+    @Test
+    fun writeAndReadWorks() {
+        assertEquals(set.blackList, filters.map {it.second})
+    }
+
+    @Test
+    fun filterContainerWorks() {
+        val container = FilterContainer().also { filters.forEach(it::addWithTag) }
+
+        assertNotNull(container[TestWebResourceRequest(Uri.parse("http://g.doubleclick.net/ads"),false, mapOf(
+            "User-Agent" to "Mozilla/5.0 (Linux; Android 9; SM-I9195I) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.204 Mobile Safari/537.36",
+            "Origin" to "https://orf.at", "Referer" to "https://orf.at/", "Accept" to "application/json, text/javascript, */*; q=0.01")).getContentRequest("https://orf.at".toUri())])
+
+        assertNull(container[TestWebResourceRequest(Uri.parse("https://orf.at/"),false, mapOf(
+            "User-Agent" to "Mozilla/5.0 (Linux; Android 9; SM-I9195I) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.204 Mobile Safari/537.36",
+            "Origin" to "https://orf.at", "Referer" to "https://orf.at/", "Accept" to "application/json, text/javascript, */*; q=0.01")).getContentRequest("https://orf.at".toUri())])
+
+    }
+
+}
+
+class TestWebResourceRequest(val url2: Uri,
+                             val isForMainFrame2: Boolean,
+                             val requestHeaders2: Map<String, String>
+                             ): WebResourceRequest {
+    override fun getUrl() = url2
+    override fun isForMainFrame() = isForMainFrame2
+    override fun getRequestHeaders() = requestHeaders2
+    override fun isRedirect() = false // not needed
+    override fun hasGesture() = false // not needed
+    override fun getMethod() = "" // not needed
+}
+
+fun WebResourceRequest.getContentRequest(pageUri: Uri) =
+    ContentRequest(url, pageUri, getContentType(pageUri), is3rdParty(url, pageUri))
+
+fun is3rdParty(url: Uri, pageUri: Uri): Boolean {
+    val hostName = url.host ?: return true
+    val pageHost = pageUri.host ?: return true
+
+    if (hostName == pageHost) return false
+
+    val ipPattern = PatternsCompat.IP_ADDRESS
+    if (ipPattern.matcher(hostName).matches() || ipPattern.matcher(pageHost).matches())
+        return true
+
+    val db = PublicSuffix.get()
+
+    return db.getEffectiveTldPlusOne(hostName) != db.getEffectiveTldPlusOne(pageHost)
 }
--- app/src/test/java/acr/browser/lightning/adblock/NoOpAdBlockerTest.kt
+++ app/src/test/java/acr/browser/lightning/adblock/NoOpAdBlockerTest.kt
@@ -1,7 +1,6 @@
 package acr.browser.lightning.adblock
 
 import android.net.Uri
-import android.webkit.WebResourceRequest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
@@ -18,32 +17,4 @@ class NoOpAdBlockerTest {
         assertThat(noOpAdBlocker.shouldBlock(request, "https://google.com")).isNull()
     }
 
-    class TestWebResourceRequest(url: Uri,
-                                 isForMainFrame: Boolean,
-                                 requestHeaders: Map<String, String>): WebResourceRequest {
-        override fun getUrl(): Uri {
-            return url
-        }
-
-        override fun isForMainFrame(): Boolean {
-            return isForMainFrame
-        }
-
-        override fun isRedirect(): Boolean {
-            return false // not needed
-        }
-
-        override fun hasGesture(): Boolean {
-            return false // not needed
-        }
-
-        override fun getMethod(): String {
-            return "" // not needed
-        }
-
-        override fun getRequestHeaders(): MutableMap<String, String> {
-            return requestHeaders
-        }
-
-    }
 }
