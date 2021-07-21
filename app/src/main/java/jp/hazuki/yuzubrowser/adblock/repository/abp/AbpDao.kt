533947cc05b20a57b7de2426750e364e3af85a9d
Helium314 <43007630+Helium314@users.noreply.github.com>
2021-07-21 08:33:30 +0200

    add StevenBlack hosts list to default blocklists

--- app/src/main/java/jp/hazuki/yuzubrowser/adblock/repository/abp/AbpDao.kt
+++ app/src/main/java/jp/hazuki/yuzubrowser/adblock/repository/abp/AbpDao.kt
@@ -71,10 +71,11 @@ class AbpDao(val context: Context) {
 
 }
 
-// pre-fill some stuff, currently only built-in easylist enabled (enable others by manipulating preferences xml)
+// add some default block lists
 const val ABP_ENTITIES = "abpEntities"
 val ABP_ENTITY_EASYLIST_BUILTIN = AbpEntity(title = "Internal List", entityId = 1, url = "fulguris://easylist", homePage = "https://easylist.to")
 val ABP_ENTITY_EASYLIST = AbpEntity(title = "EasyList", entityId = 2, url = "https://easylist.to/easylist/easylist.txt", homePage = "https://easylist.to")
 val ABP_ENTITY_EASYPRIVACY = AbpEntity(title = "EasyPrivacy", entityId = 3, url = "https://easylist.to/easylist/easyprivacy.txt", homePage = "https://easylist.to")
 val ABP_ENTITY_URLHAUS = AbpEntity(title = "Malicious URL Blocklist", entityId = 4, url = "https://curben.gitlab.io/malware-filter/urlhaus-filter-agh-online.txt", homePage = "https://gitlab.com/curben/urlhaus-filter")
-val ABP_DEFAULT_ENTITIES = setOf(/*ABP_ENTITY_EASYLIST_BUILTIN.toString(), */ABP_ENTITY_EASYLIST.toString(), ABP_ENTITY_EASYPRIVACY.toString(), ABP_ENTITY_URLHAUS.toString())
+val ABP_ENTITY_STEVEN_BLACK = AbpEntity(title = "StevenBlack hosts list", entityId = 5, url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts", homePage = "https://github.com/StevenBlack/hosts", enabled = false)
+val ABP_DEFAULT_ENTITIES = setOf(/*ABP_ENTITY_EASYLIST_BUILTIN.toString(), */ABP_ENTITY_EASYLIST.toString(), ABP_ENTITY_EASYPRIVACY.toString(), ABP_ENTITY_URLHAUS.toString(), ABP_ENTITY_STEVEN_BLACK.toString())
