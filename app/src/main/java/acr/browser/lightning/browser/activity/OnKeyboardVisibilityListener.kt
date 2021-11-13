be7a3a12ac57ff36d49fe417fe61c393fb0e5558
Jamal <jamal2362@googlemail.com>
2021-11-13 09:48:52 +0100

    Add second navigation bar
    Issue: https://github.com/Slion/Fulguris/issues/265

---
+++ app/src/main/java/acr/browser/lightning/browser/activity/OnKeyboardVisibilityListener.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2020-2021 Jamal Rothfuchs
+ * Copyright © 2020-2021 Stéphane Lenclud
+ * Copyright © 2015 Anthony Restaino
+ */
+
+package acr.browser.lightning.browser.activity
+
+interface OnKeyboardVisibilityListener {
+    fun onVisibilityChanged(visible: Boolean)
+}
