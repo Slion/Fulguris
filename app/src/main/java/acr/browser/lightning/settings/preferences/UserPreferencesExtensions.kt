4789188214a5bb37b00eb26e3105680170bce5b4
Helium314 <helium314@disroot.org>
2021-12-27 12:29:22 +0100

    remove device name from default user agent

--- app/src/main/java/acr/browser/lightning/settings/preferences/UserPreferencesExtensions.kt
+++ app/src/main/java/acr/browser/lightning/settings/preferences/UserPreferencesExtensions.kt
@@ -12,11 +12,8 @@ fun UserPreferences.userAgent(application: Application): String =
         // WebSettings default identifies us as WebView and as WebView Google is preventing us to login to its services.
         // Clearly we don't want that so we just modify default user agent by removing the WebView specific parts.
         // That should make us look like Chrome, which we are really.
-        1 -> {
-            var userAgent = Regex(" Build/.+; wv").replace(WebSettings.getDefaultUserAgent(application),"")
-            userAgent = Regex("Version/.+? ").replace(userAgent,"")
-            userAgent
-        }
+        1 -> "Mozilla/5.0 (Linux; Android ${Build.VERSION.RELEASE})" +
+                WebSettings.getDefaultUserAgent(application).substringAfter(")")
         2 -> WINDOWS_DESKTOP_USER_AGENT
         3 -> LINUX_DESKTOP_USER_AGENT
         4 -> MACOS_DESKTOP_USER_AGENT
