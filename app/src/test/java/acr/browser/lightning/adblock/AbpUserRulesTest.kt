b87ee1b3f7179e120a9247b92d636650cfefc79a
Helium <helium314@disroot.org>
2022-02-03 08:38:38 +0100

    add test for user rules

---
+++ app/src/test/java/acr/browser/lightning/adblock/AbpUserRulesTest.kt
@@ -0,0 +1,27 @@
+package acr.browser.lightning.adblock
+
+import acr.browser.lightning.database.adblock.UserRulesRepository
+import androidx.core.net.toUri
+import org.junit.Assert
+import org.junit.Test
+
+class AbpUserRulesTest {
+    private val abpUserRules = AbpUserRules(NoUserRulesRepository())
+
+    @Test
+    fun block() {
+        abpUserRules.allowPage("http://page.com/something".toUri(), add = true)
+        // now page.com should be explicitly allowlisted
+        Assert.assertTrue(abpUserRules.isAllowed("http://page.com/otherthing".toUri()))
+        Assert.assertFalse(abpUserRules.isAllowed("http://page2.com/otherthing".toUri()))
+        Assert.assertFalse(abpUserRules.isAllowed("http://test.page.com/otherthing".toUri()))
+    }
+
+}
+
+private class NoUserRulesRepository: UserRulesRepository {
+    override fun addRules(rules: List<UnifiedFilterResponse>) {}
+    override fun removeAllRules() {}
+    override fun removeRule(rule: UnifiedFilterResponse) {}
+    override fun getAllRules() = listOf<UnifiedFilterResponse>()
+}
