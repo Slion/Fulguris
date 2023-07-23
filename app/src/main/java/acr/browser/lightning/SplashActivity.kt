c2474a95cd543aaa888fbb6f48fa1d4ba416123e
Stéphane Lenclud <github@lenclud.com>
2023-07-23 17:51:13 +0200

    Splash screen Android 12 crash fix
    Just skip the splash screen below Android 13.

--- app/src/main/java/acr/browser/lightning/SplashActivity.kt
+++ app/src/main/java/acr/browser/lightning/SplashActivity.kt
@@ -22,46 +22,64 @@
  
 package acr.browser.lightning
 
-import acr.browser.lightning.browser.activity.ThemedBrowserActivity
 import acr.browser.lightning.locale.LocaleAwareActivity
-import acr.browser.lightning.settings.preferences.UserPreferences
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import javax.inject.Inject
-import javax.inject.Singleton
 
 /**
  * Still needed a splash screen activity as the SplashScreen API would not play well with our themed activity.
  * We just could not get our theme override to work then.
  */
+@SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class SplashActivity @Inject constructor(): LocaleAwareActivity() {
-
-    val mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Setup our splash screen
-        // See: https://developer.android.com/guide/topics/ui/splash-screen
-        val splashScreen = installSplashScreen()
-        splashScreen.setOnExitAnimationListener {
-            // Callback once our splash screen is done
-            // Splash screen duration is define in our style in Theme.App.SplashScreen
-            // NOTE: Though it does not seem to be working, therefore we cab use the post delayed below
-
-            // Remove our splash screen, though not needed since we are closing this activity anyway
-            //it.remove()
-            // Close this activity, with a defensive delay for smoother transitions on slower devices
-            //finish()
+        // Splashscreen actually crashes on Android 12 when targeting Android 13
+        // https://stackoverflow.com/questions/72390747/crash-activity-client-record-must-not-be-null-to-execute-transaction-item-only
+        // See: https://issuetracker.google.com/issues/210886009
+        //val skip = Build.VERSION.SDK_INT == Build.VERSION_CODES.S || Build.VERSION.SDK_INT == Build.VERSION_CODES.S_V2
+        // Just skip it always it looks the same without it anyway
+        // Skipping it on Samsung Galaxy A22 with Android 13 looked ugly
+        val skip = Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU
+        if (skip) {
+            // Need to do that when not using splash screen otherwise we crash with:
+            // java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity.
+            setTheme(R.style.Theme_App_DayNight)
         }
 
-        // Put this here as above in the callback it did not work on Android 12
-        // It would be too much to ask Google engineer to test their code across Android versions…
-        mHandler.postDelayed({
+        if (!skip) {
+            // Setup our splash screen
+            // See: https://developer.android.com/guide/topics/ui/splash-screen
+            val splashScreen = installSplashScreen()
+            splashScreen.setOnExitAnimationListener {
+                // Callback once our splash screen is done
+                // Splash screen duration is define in our style in Theme.App.SplashScreen
+                // NOTE: Though it does not seem to be working, therefore we cab use the post delayed below
+
+                // Remove our splash screen, though not needed since we are closing this activity anyway
+                //it.remove()
+                // Close this activity, with a defensive delay for smoother transitions on slower devices
+                //finish()
+            }
+        }
+
+        //setContentView(R.layout.activity_splash)
+
+        findViewById<View>(android.R.id.content).
+            // Put this here as above in the callback it did not work on Android 12
+            // It would be too much to ask Google engineer to test their code across Android versions…
+        postDelayed({
+            Timber.d("SplashScreen skipped: $skip")
             // Just start our main activity now for fastest loading
             // TODO: check if we need onboarding
             // Launch main activity
@@ -70,9 +88,8 @@ class SplashActivity @Inject constructor(): LocaleAwareActivity() {
             //
             finish()
         },0)
-
-        setContentView(R.layout.activity_splash)
     }
+
 
     override fun onLocaleChanged() {
         //TODO("Not yet implemented")
