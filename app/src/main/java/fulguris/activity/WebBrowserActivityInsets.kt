e086b2ca88102c31134ad926ac17f034c1e1d242
Stéphane Lenclud <github@lenclud.com>
2025-10-17 15:51:40 +0200

    New file for web browser activity insets handler

---
+++ app/src/main/java/fulguris/activity/WebBrowserActivityInsets.kt
@@ -0,0 +1,85 @@
+package fulguris.activity
+
+import android.os.Build
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import fulguris.di.configPrefs
+import timber.log.Timber
+
+/**
+ * Deal with screen insets, cutouts, notches and such
+ */
+fun WebBrowserActivity.applyWindowInsets(view: View, windowInsets: WindowInsetsCompat) {
+
+    // That's still needed before API 35, it was at least needed for Android 13 on Samsung Galaxy A22
+    // See: https://github.com/Slion/Fulguris/issues/686
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val cutoutMode = if (configPrefs.useCutoutArea) LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS else LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+        if (window.attributes.layoutInDisplayCutoutMode != cutoutMode) {
+            // We did not seem to be able to apply that without restarting the activity,
+            window.attributes.layoutInDisplayCutoutMode = cutoutMode
+            // This makes sure the newly set cutout mode is applied, without restarting activity I guess
+            window.attributes = window.attributes
+            // TODO adjust attributes for all our dialog windows
+        }
+    }
+
+    val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+    Timber.d("System insets: $systemBars")
+    //val imeVisible = windowInsets.isVisible(WindowInsetsCompat.Type.ime())
+    val imeHeight = windowInsets.getInsets(WindowInsetsCompat.Type.ime()).bottom
+
+    val gestureInsets = windowInsets.getInsets(WindowInsetsCompat.Type.systemGestures())
+    Timber.d("Gesture insets: $gestureInsets")
+
+    val cutout = windowInsets.getInsets(WindowInsetsCompat.Type.displayCutout())
+    Timber.d("Cutout insets: $cutout")
+
+    // Workout our insets according to configuration
+    // From API 35 layoutInDisplayCutoutMode is useless, instead of relying on the root window we need to adjust our layout ourselves
+    val insets = windowInsets.getInsets(if (!configPrefs.useCutoutArea) {
+        // Exclude display cutout
+        WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+    }
+    else {
+        // Only exclude system bars
+        WindowInsetsCompat.Type.systemBars()
+    })
+
+    view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        // Don't apply vertical margins here as it would break our drawers status bar color
+        // Apply horizontal margin to our root view so that we fill the cutout in on Honor Magic V2
+        leftMargin = insets.left //+ gestureInsets.left
+        rightMargin = insets.right //+ gestureInsets.right
+        // Make sure our UI does not get stuck below the IME virtual keyboard
+        // TODO: Do animation synchronization, see: https://developer.android.com/develop/ui/views/layout/sw-keyboard#synchronize-animation
+        bottomMargin = imeHeight
+    }
+
+    iBinding.uiLayout.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        // Apply vertical margins for status and navigation bar to our UI layout
+        // Thus the drawers are still showing below the status bar
+        topMargin = insets.top
+        bottomMargin = insets.bottom //+ gestureInsets.bottom
+        //leftMargin = gestureInsets.left
+        //rightMargin = gestureInsets.right
+    }
+
+    iBinding.leftDrawerContent.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        // Apply vertical margins for status and navigation bar to our drawer content
+        // Thus drawer content does not overlap with system UI
+        topMargin = insets.top
+        bottomMargin = insets.bottom
+    }
+
+    iBinding.rightDrawerContent.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        // Apply vertical margins for status and navigation bar to our drawer content
+        // Thus drawer content does not overlap with system UI
+        topMargin = insets.top
+        bottomMargin = insets.bottom
+    }
+}
